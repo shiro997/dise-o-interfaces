a63f9667306f33b9484b8512f5e5b86fe212348b
Mateo Blanco <mateo997@outlook.com>
2019-09-09 19:49:43 -0500

    some

---
+++ MateoBlanco/scripts/js/home.js
@@ -0,0 +1,22 @@
+var clean = function(){
+	document.getElementById("code").value="";
+	document.getElementById("pass").value="";
+}
+
+var redirect = function(){
+	var inputCode = parseInt(document.getElementById("code").value);
+	var inputPass = document.getElementById("pass").value;
+	var baseUser = {
+		codigo: 506151060,
+		password: "h3lctcep0"
+	};
+	if(inputCode == baseUser.codigo && inputPass == baseUser.password)
+	{
+		$(loacation.assign("MateoBlanco/pages/User.html"));
+		//window.location = "../MateoBlanco/pages/User.html";
+	}else{
+		alert("datos erróneos");
+		$(loacation.assign("MateoBlanco/index.html"));
+		//window.location = "../MateoBlanco/index.html";
+	}
+}
